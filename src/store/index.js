30382a1e98c4d47cd5908c6cae42f7836c0ce350
gnich12 <jinto12@gmail.com>
2019-03-22 23:54:52 -0700

    create storage index file
    main entry point for storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import recipe from './recipes'
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    recipe
   }
 })
