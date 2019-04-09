c1a363add5048812e4e97a48d7ff4e1d8cfcb606
louischrisorona <louischrisorona@gmail.com>
2019-04-09 14:42:44 -0700

    Reduced filtering options

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -99,21 +99,21 @@ export default {
       page: 1,
       numItemPerPage: 10,
       filterOptions: [
-        { id: '1', tag: 'balanced', checked: false },
+        { id: '1', tag: 'balanced' },
         { id: '2', tag: 'high-protein' },
         { id: '3', tag: 'high-fiber' },
         { id: '4', tag: 'low-fat' },
         { id: '5', tag: 'low-carb' },
-        { id: '6', tag: 'low-sodium' },
-        { id: '7', tag: 'vegan' },
-        { id: '8', tag: 'vegetarian' },
-        { id: '9', tag: 'dairy-free' },
-        { id: '10', tag: 'low-sugar' },
-        { id: '11', tag: 'low-fat-abs' },
-        { id: '12', tag: 'sugar-conscious' },
-        { id: '13', tag: 'fat free' },
-        { id: '14', tag: 'gluten free' },
-        { id: '15', tag: 'wheat free' }
+        { id: '6', tag: 'low-sodium' }
+        // { id: '7', tag: 'vegan' },
+        // { id: '8', tag: 'vegetarian' },
+        // { id: '9', tag: 'dairy-free' },
+        // { id: '10', tag: 'low-sugar' },
+        // { id: '11', tag: 'low-fat-abs' },
+        // { id: '12', tag: 'sugar-conscious' },
+        // { id: '13', tag: 'fat free' },
+        // { id: '14', tag: 'gluten free' },
+        // { id: '15', tag: 'wheat free' }
       ],
       tagged: []
     }
