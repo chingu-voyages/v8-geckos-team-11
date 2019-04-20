ca42051e60ecee22dee4f9428bec487b8ee5baa5
gnich12 <jinto12@gmail.com>
2019-04-20 09:36:20 -0700

    initial faq page work

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <ShoppingList/>
-      <v-btn flat href="#FAQ">
+      <v-btn flat @click="openFaq">
         <span class="mr-2">FAQ</span>
       </v-btn>
     </v-toolbar>
@@ -18,6 +18,48 @@
 <!-- Search Results -->
       <SearchResults/>
     </v-content>
+    <v-dialog
+      v-model="openFaqDialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+      scrollable
+    >
+     <v-card>
+       <v-toolbar card dark color="primary">
+          <v-btn icon dark @click="openFaqDialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>FAQ</v-toolbar-title>
+       </v-toolbar>
+       <v-card-text>
+         <v-container>
+           <v-layout row wrap>
+             <v-flex xs12>
+               <h2 class="display-3">In the Pan FAQ</h2>
+               <div>
+                 <p>Here you will find the common answers to top frequently questions</p>
+               </div>
+             </v-flex>
+             <v-flex xs12>
+               <h2>Question# 1</h2>
+               <div>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam,
+                   expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+               </div>
+             </v-flex>
+             <v-flex xs12>
+               <h2>Question# 2</h2>
+               <div>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam,
+                    ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+               </div>
+             </v-flex>
+           </v-layout>
+         </v-container>
+       </v-card-text>
+     </v-card>
+    </v-dialog>
   </v-app>
 </template>
 <script>
@@ -30,6 +72,16 @@ export default {
     SearchBar,
     SearchResults,
     ShoppingList
+  },
+  data () {
+    return {
+      openFaqDialog: false
+    }
+  },
+  methods: {
+    openFaq () {
+      this.openFaqDialog = true
+    }
   }
 }
 </script>
