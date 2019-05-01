bbdf07936c04238d3e9045d21cc60ff6742bee22
Simon X <simonxiong11@gmail.com>
2019-04-30 23:25:40 -0700

    display About dialogue with content

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
         <span @click="reloadPage" class="logo">In The PAN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+<!-- About start -->
+      <v-btn flat @click.stop="openAbout">
+        <span class="mr-2" >About</span>
+      </v-btn>
+<!-- About end -->
       <div class="hidden-sm-and-down">
         <ShoppingList/>
       </div>
@@ -20,6 +25,51 @@
 <!-- Search Results -->
       <SearchResults ref="drawer"/>
     </v-content>
+<!-- About content start-->
+     <v-dialog
+      v-model="openAboutDialog"
+      fullscreen
+      transition="dialog-bottom-transition"
+      scrollable
+      color="blue"
+    >
+     <v-card>
+       <v-toolbar dark color="primary" card>
+          <v-btn icon dark @click="openAboutDialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>About</v-toolbar-title>
+       </v-toolbar>
+       <v-card-text>
+         <v-container>
+           <v-layout row wrap>
+             <v-flex xs12>
+               <h2 class="display-2">Welcome</h2>
+               <div>
+                 <p>In the Pan offers its users a ton of recipe ideas base on a single food item</p>
+               </div>
+                <v-divider></v-divider>
+               <div>
+                 <h3>The Community:</h3>
+                 <p>Chingu Voyage-8:</p>
+               </div>
+                <v-divider></v-divider>
+               <div>
+                 <h3>Special Thanks to:</h3>
+                 <p>Edamam: </p>
+               </div>
+                <v-divider></v-divider>
+               <div>
+                 <h3>Contributors:</h3>
+                 <p>Elizabeth, Alvaro, Chris, Simon</p>
+               </div>
+             </v-flex>
+           </v-layout>
+         </v-container>
+       </v-card-text>
+     </v-card>
+    </v-dialog>
+<!-- About content end -->
     <v-dialog
       v-model="openFaqDialog"
       fullscreen
@@ -70,6 +120,7 @@ export default {
   data () {
     return {
       openFaqDialog: false,
+      openAboutDialog: false,
       drawer: false,
       faq: faq
     }
@@ -80,6 +131,9 @@ export default {
     },
     openFaq () {
       this.openFaqDialog = true
+    },
+    openAbout () {
+      this.openAboutDialog = true
     }
   }
 }
